f874a589a9f5f5c04ce65328c40effa782005fc9
Priyanshu Raj <priyanshuraj9888@gmail.com>
2022-04-10 15:11:32 +0530

    hh

--- web_project/settings.py
+++ web_project/settings.py
@@ -131,3 +131,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
